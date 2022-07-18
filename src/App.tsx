c19c13c0055d571d0972ac2b23dfdad77c2dac75
DrMustafaH <mustafaharith@gmail.com>
2022-07-17 19:01:16 -0700

    Before Cleaning Code

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import List from 'rc-virtual-list';
-
+import InfiniteScroll from "react-infinite-scroll-component";
+import Modal from "./Modal";
 import "./App.css";
 
-type SceneType = {
+export type SceneType = {
   key: string;
   keywordKeys: string[];
   propCat: string;
@@ -12,15 +12,11 @@ type SceneType = {
 };
 
 function App() {
-  // const count = 30;
-  const increment = 30
-  // const number = 30;
+  const count = 30;
   const [sceneList, setSceneList] = useState<Array<SceneType>>([]);
   const [displayList, setDisplayList] = useState<Array<SceneType>>([]);
-  // const [pageIndex, setPageIndex] = useState<number>(count);
   const [showModal, setShowModal] = useState<boolean>(false);
-  const [selectedScene , setSelectedScene] = useState<string>('')
-  const [currentIndex , setCurrentIndex] = useState<number>(30)
+  const [selectedScene, setSelectedScene] = useState<SceneType>();
 
   useEffect(() => {
     async function fetchData() {
@@ -32,82 +28,67 @@ function App() {
         "https://img.pixton.com/data/comic-scene-group-data.json"
       );
       setSceneList(sceneGroups);
-      setDisplayList(sceneGroups.slice(0, 30));
+      setDisplayList(sceneGroups.slice(0, count));
     }
     fetchData();
   }, []);
 
-  // function handleScroll(e: any) {
-  //   const bottom =
-  //     e.target.scrollHeight - e.target.scrollTop === e.target.clientHeight;
-  //   console.log(bottom);
-  //   if(bottom) { 
-  //     const extendList =  sceneList.slice(pageIndex, pageIndex + count)
-  //     setPageIndex(pageIndex + count)
-  //     setDisplayList([...displayList, ...extendList])
-  //   }
-  // }
-
-  const handleImgClick = (sceneUrl: string) => {
-    setSelectedScene(sceneUrl)
-    setShowModal(!showModal)
-  }
-
-  const handlePrev = () => {
-    setCurrentIndex(currentIndex - count)
-  }
-  const handleNext = () => {
-    setDisplayList(sceneList.slice(currentIndex, currentIndex + increment))
-    setCurrentIndex(currentIndex + increment)
-  }
+  const handleImgClick = async (scene: SceneType) => {
+    document.body.style.overflow = "hidden";
+    setSelectedScene(scene);
+    setShowModal(!showModal);
+  };
 
   const closeSelectedScene = () => {
-    setSelectedScene('')
-    setShowModal(!showModal)
-  }
-  console.log(sceneList);
+    document.body.style.overflow = "auto";
+    setSelectedScene(undefined);
+    setShowModal(!showModal);
+  };
+
+  const handleScroll = () => {
+    setDisplayList([
+      ...displayList,
+      ...sceneList.slice(
+        displayList.length - 1,
+        displayList.length - 1 + count
+      ),
+    ]);
+  };
+  const hasMore = displayList.length < sceneList.length;
   return (
     <div className="App">
-      <div>
-      <div>Gallery List</div>
-      {/* <List data={sceneList} height={1000} itemHeight={300} itemKey="id" className="mustafa-list">
-              {scene => <div key={scene.key}>
-                <img className='list-img' alt='scene' height={10}src={scene.url} onClick={()=>handleImgClick(scene.url)}></img>
-              </div>}
-      </List> */}
-      </div>
-      <div
-        id="gallery-grid"
-        // onScroll={handleScroll}
-        // style={{ overflow: "scroll", width: "100wh", height: "1100px" }}
+      <InfiniteScroll
+        dataLength={displayList.length}
+        next={handleScroll}
+        hasMore={hasMore}
+        loader={<h4>Loading...</h4>}
       >
-        {displayList.map((scene) => {
-          return (
-            <div key={scene.key}>
-              <img
-                alt="scene"
-                src={scene.url}
-                width="200"
-                height="200"
-              ></img>
-            </div>
-          );
-        })}
-      </div>
-      <div>
-        <button onClick={handlePrev}>
-          Prev
-        </button>
-        <button onClick={handleNext}>
-          Next
-        </button>
-      </div>
-      <div>
-      {showModal && <div className='selected-scene'>
-          <button className='close-selected-scene' onClick={closeSelectedScene}>✕</button>
-          <img src={selectedScene} alt='selectedImg'></img>
-        </div>}
-        </div>  
+        <div>
+          <div id="gallery-grid">
+            {displayList.map((scene, index) => {
+              return (
+                <div key={`${scene.key}-${index}`}>
+                  <img
+                    className="list-img"
+                    alt="scene"
+                    src={scene.url}
+                    width="200"
+                    height="200"
+                    onClick={() => handleImgClick(scene)}
+                  ></img>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </InfiniteScroll>
+      {selectedScene && (
+        <Modal
+          showModal={showModal}
+          scene={selectedScene}
+          onClose={closeSelectedScene}
+        />
+      )}
     </div>
   );
 }
---
+++ src/Modal.tsx
@@ -0,0 +1,36 @@
+import { SceneType } from "./App";
+
+function Modal({
+  showModal,
+  scene,
+  onClose,
+}: {
+  showModal: boolean;
+  scene: SceneType;
+  onClose: () => void;
+}) {
+  const getRandomColor = () => {
+    const opacity = "0.8";
+    let color = "rgba(";
+    for (var i = 0; i < 3; i++) {
+      color += Math.floor(Math.random() * 255) + ",";
+    }
+    color += opacity + ")";
+    return color;
+  };
+  let randomColor = getRandomColor();
+  return (
+    <div className="modal" style={{ backgroundColor: randomColor }}>
+      {showModal && (
+        <div className="selected-scene">
+          <button className="close-selected-scene" onClick={onClose}>
+            ✕
+          </button>
+          <img src={scene.url} alt="selectedImg"></img>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Modal;
